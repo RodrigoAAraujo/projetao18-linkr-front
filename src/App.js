462c9a6e853d8f07d09b954c753cc3360bdcaf90
Dannepi <dannepi.dev@gmail.com>
2023-01-03 21:37:15 -0300

    creation of 'pages' folder, reorganization of index.js:'no strict mode, since it bugs useEffect' and app.js:'simple app.js configuration with Router-dom and styled-components'.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import styled, { createGlobalStyle } from "styled-components";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignupPage";
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <AppStyle>
+      <GlobalStyle/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/signup" element={<SignUpPage/>}/>
+        </Routes>
+      </BrowserRouter>
+    </AppStyle>
   );
 }
 
 export default App;
+
+const GlobalStyle = createGlobalStyle`
+  *{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+`
+const AppStyle = styled.div`
+  width: 100vw;
+  height: 100vh;
+`
