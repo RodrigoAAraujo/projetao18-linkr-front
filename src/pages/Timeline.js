322e67659fc407b9005f96e33a237eaa4087f916
MarcusCastro0701 <marcus.castro0701@gmail.com>
2023-01-13 15:39:56 -0300

    fix: timeline.js

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -313,6 +313,23 @@ align-self: flex-end;
     opacity: 0.8;
 }
 `
+
+const LoadButton = styled.button`
+
+width: 100%;
+height: 61px;
+font-family: 'Lato';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 19px;
+
+color: #FFFFFF;
+
+background: #1877F2;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 16px;
+`
 export {Container, EnglobaConteudo, Title};
 
 
